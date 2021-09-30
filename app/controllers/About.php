68473090cb72199f9a5e45e51fb5d37d4bf54380
restu1909 <58680735+restu1909@users.noreply.github.com>
2021-09-30 08:48:09 +0700

    menyelesaikan video view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,23 @@
 <?php
 
-class About{
+class About extends Controller{
 
-    public function index($nama = 'Restu', $pekerjaan = 'mahasiswa'){
-        echo "halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    public function index($nama = 'Restu', $pekerjaan = 'mahasiswa', $umur = 21)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'about me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
-    public function page(){
-        echo 'About/page';
+    public function page()
+    {
+        $data['judul'] = 'pages';
+        $this->view('templates/header');
+        $this->view('about/page', $data);
+        $this->view('templates/footer');
     }
 }
 
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+
+    <h1>about me</h1>
+    <p>
+        halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?>, saya adalah seorang <?= $data['pekerjaan'];?>.
+    </p>
